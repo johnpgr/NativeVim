c2f37c3ba6cecd95ebc2617c17c1e23b8ae371fb
johnpgr <jp.greidinger@hotmail.com>
2025-05-05 16:05:18 -0300

    make highlights.lua consistent

--- lua/core/highlights.lua
+++ lua/core/highlights.lua
@@ -1,17 +1,15 @@
 local function hl(name, opts)
-    local hl_group = vim.api.nvim_create_namespace(name)
-    vim.api.nvim_set_hl(0, name, opts)
-    return hl_group
+	local hl_group = vim.api.nvim_create_namespace(name)
+	vim.api.nvim_set_hl(0, name, opts)
+	return hl_group
 end
 
+-- Remove normal background color
 hl("Normal", { bg = "none" })
 
--- Force diagnostics to use undercurls
-vim.cmd [[ 
-    hi DiagnosticUnderlineError gui=undercurl
-    hi DiagnosticUnderlineHint gui=undercurl
-    hi DiagnosticUnderlineInfo gui=undercurl
-    hi DiagnosticUnderlineOk gui=undercurl
-    hi DiagnosticUnderlineWarn gui=undercurl
-]]
-
+-- Force undercurls in diagnostics
+hl("DiagnosticUnderlineError", { undercurl = true })
+hl("DiagnosticUnderlineHint", { undercurl = true })
+hl("DiagnosticUnderlineInfo", { undercurl = true })
+hl("DiagnosticUnderlineOk", { undercurl = true })
+hl("DiagnosticUnderlineWarn", { undercurl = true })
